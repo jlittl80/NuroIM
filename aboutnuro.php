648dfd0851a74810b348f8302b29c4fe0f565039
Jordie <jordie.little@live.com>
2019-06-08 16:45:49 +1000

    Initial commit

---
+++ aboutnuro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About NuroIM :: NuroIM</title>
+<script src="js/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#p2").addClass("selected a");
+});
+</script>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/banner.css" rel="stylesheet" type="text/css" />
+<link href="css/footer.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body style="background-color:#7d7d7d; margin:0; padding:0;">
+<div id="wrap">
+<div id="main">
+<?php
+	include 'html/banner.html';
+?>
+<?php
+	include 'html/aboutnuro.html';
+?>
+</div>
+</div>
+<?php
+	include 'html/footer.html';
+?>
+</body>
+</html>
